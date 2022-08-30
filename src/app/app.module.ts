65f39c6d420a50401e8cbd44c0dcce8d89176bea
AhmadReza Mozaffary <AhmadRezaM@systemgroup.net>
2022-08-30 20:13:26 +0430

    Seprating routing related codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,27 +10,8 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
-import { RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-
-const appRoute: Routes = [
-  { path: "", component: HomeComponent },
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [{ path: ":name/:id", component: UserComponent }],
-  },
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent },
-    ],
-  },
-  { path: "page-not-found", component: PageNotFoundComponent },
-  { path: "**", redirectTo: "page-not-found" },
-];
+import { AppRoutingModule } from "./app.routing.module";
 
 @NgModule({
   declarations: [
@@ -43,7 +24,7 @@ const appRoute: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoute)],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
