d610c64532b5562c7d682a509dddda1b9b919f18
AhmadReza Mozaffary <AhmadRezaM@systemgroup.net>
2022-08-31 07:55:25 +0430

    Adding fake auth functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private authService: AuthService) {}
+
+  loggedIn = false;
+
+  onLogin() {
+    this.authService.logging("in");
+    this.loggedIn = true;
+  }
+
+  onLogout() {
+    this.authService.logging("out");
+    this.loggedIn = false;
+  }
+}
